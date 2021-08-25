ea19b43a767c57884cf8bd575f935c31d3de1df6
gabrielbins <gabriel.the.bins@gmail.com>
2021-08-25 14:43:30 -0400

    fix1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-fixacao/scripts.js
@@ -0,0 +1,11 @@
+const myName = "Gabriel Bins" ;
+
+const birthCity = "Pimenta Bueno" ;
+
+let birthYear = 1996 ;
+
+birthYear = 2030 ;
+
+console.log (myName)
+console.log (birthCity)
+console.log (birthYear)
