c62e1761f5eb5b2e26a7e607c3ea7e580d81961c
gabrielbins <gabriel.the.bins@gmail.com>
2021-08-25 17:30:50 -0400

    fix4

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/exercicio-fixacao/fix4.js
@@ -0,0 +1,19 @@
+let condicao = "irineu"
+
+switch (condicao) {
+    case "aprovada":
+        console.log("Parabéns, você foi aprovada(o)!")
+        break;
+    
+    case "lista":
+        console.log("Você está na nossa lista de espera")
+        break;
+
+    case "reprovada":
+        console.log ("Você foi reprovada(o)")
+        break;
+
+    default:
+        console.log("valor não se aplica")
+
+}
