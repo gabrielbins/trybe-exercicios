a05ecdfff5f15d25f7587f4432f9b63d927cf1cc
gabrielbins <gabriel.the.bins@gmail.com>
2021-08-26 10:58:05 -0400

    ex5.

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex5.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-1-javascript-primeiros-passos/ex5.js
@@ -1,13 +1,15 @@
 const a = 50;
-const b = 100;
+const b = -5;
 const c = 100;
 let verificacao;
 
-if (a+b+c === 180){
+if (a<0||b<0||c<0){
+    console.log("error")
+}
+else if (a+b+c === 180) {
     verificacao = true
 }
 else {
     verificacao = false
 }
-
 console.log(verificacao)
