1cc7cfbf7199bd93cce4335d6d811da1cc17baa7
gabrielbins <gabriel.the.bins@gmail.com>
2021-08-26 15:10:48 -0400

    exercicio7

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,15 +1,10 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-let numerosImpares = 0
+let menor = numbers[0];
 
 for (let numeros of numbers){
-    if (numeros%2 != 0){
-        numerosImpares += 1
+    if (numeros < menor){
+        menor = numeros
     }
 }
 
-if (numerosImpares === 0){
-    console.log("Nenhum valor impar encontrado")
-} 
-else {
-    console.log(numerosImpares)
-}
+console.log(menor)
