14c5b4dda1f7fb3a53b10ef222417a109497dad2
gabrielbins <gabriel.the.bins@gmail.com>
2021-08-26 14:49:47 -0400

    exercicio4

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,8 +1,15 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
 let media = 0;
+
 for (let numeros of numbers){
    soma += numeros;
 }
 media = soma/numbers.length;
-console.log(media)
+
+if (media > 20) {
+    console.log("Valor maior que 20")
+}
+else {
+    console.log("Valor menor que 20")
+}
