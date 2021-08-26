89982e0038b3315927a5ae811ba0c8a904b88dbd
gabrielbins <gabriel.the.bins@gmail.com>
2021-08-26 14:44:38 -0400

    exercicio3

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/exercicios.js
@@ -1,8 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
 let soma = 0;
-
+let media = 0;
 for (let numeros of numbers){
    soma += numeros;
 }
-
-console.log(soma)
+media = soma/numbers.length;
+console.log(media)
