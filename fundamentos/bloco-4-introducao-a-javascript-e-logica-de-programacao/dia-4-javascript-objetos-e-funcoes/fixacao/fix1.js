5bb6c64cc416bc3b1f69c99b9f93294981212f0b
gabrielbins <gabriel.the.bins@gmail.com>
2021-08-30 14:42:43 -0400

    fixacao 1

---
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/fixacao/fix1.js
@@ -0,0 +1,15 @@
+let names = 'Marta';
+let lastName = 'Silva';
+let age = 34;
+let medals = { 
+    golden: 2, 
+    silver: 3 
+};
+
+let bestInTheWorld = [2006, 2007, 2008, 2009, 2010, 2018]
+
+console.log ("A jogadora " + names + lastName +" tem "+ age + " anos de idade.")
+
+console.log ("A jogadora " + names + lastName + " foi eleita a melhor do mundo por " + bestInTheWorld.length + " vezes" )
+
+console.log ("A jogadora possui " + medals.golden + " medalhas de ouro e " + medals.silver + " medalhas de prata")
