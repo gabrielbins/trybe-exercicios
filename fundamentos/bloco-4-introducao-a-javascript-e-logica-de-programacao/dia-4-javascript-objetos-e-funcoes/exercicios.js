7337e4f80dcfbe0e51cc0c7d07e469e3d5e39e61
gabrielbins <gabriel.the.bins@gmail.com>
2021-08-31 08:48:43 -0400

    exercicio 5

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/exercicios.js
@@ -2,9 +2,25 @@ let info = {
     personagem: 'Margarida',
     origem: 'Pato Donald',
     nota: 'Namorada do personagem principal nos quadrinhos do Pato Donald',
+    recorrente: "Sim"
   };
-  info["recorrente"] = "Sim"
+  
+  let info2 = {
+    personagem: 'Tio Patinhas',
+    origem: "Christmas on Bear Mountain, Dell's Four Color Comics #178",
+    nota: 'O último MacPatinhas',
+    recorrente: "Sim"
+};
 
- for (let keys in info){
-     console.log(info[keys])
- }
+for (let keys in info) {
+    if (
+        keys === 'recorrente' &&
+        info[keys] === 'Sim' &&
+        info2[keys] === 'Sim'
+     ){
+         console.log( 'Ambos recorrentes')
+        }
+        else {
+            console.log(info[keys] + " e " + info2[keys])
+        }
+}
